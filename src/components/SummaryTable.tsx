a11ecb9930d3ccb5b25fb3f9517b792738e6c996
Rodrigo P. Dias <rodrigopdias@hotmail.com>
2023-01-29 18:20:00 -0300

    adjusting padding in the SummaryTable

--- src/components/SummaryTable.tsx
+++ src/components/SummaryTable.tsx
@@ -30,7 +30,7 @@ export function SummaryTable() {
   }, []);
 
   return (
-    <div className="w-full flex overflow-x-auto scrollbar pb-2">
+    <div className="w-full flex overflow-x-auto scrollbar py-2">
       <div className="grid grid-rows-7 grid-flow-row gap-3">
         {weekDays.map((weekDay, i) => {
           return (
