9d9448fe53711d76419d4bab23ac0f917f9df788
Rodrigo P. Dias <rodrigopdias@hotmail.com>
2023-02-02 02:00:05 -0300

    adding the feature to receive notifications from the backend using a service worker

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,33 @@ import { Header } from "./components/Header";
 import { SummaryTable } from "./components/SummaryTable";
 import "./styles/global.css";
 import "./lib/dayjs";
+import { api } from "./lib/axios";
 
-navigator.serviceWorker.register("service-worker.js");
+navigator.serviceWorker
+  .register("service-worker.js")
+  .then(async (serviceWorker) => {
+    let subscription = await serviceWorker.pushManager.getSubscription();
+
+    if (!subscription) {
+      const publicKeyResponse = await api.get("/push/public_key");
+
+      subscription = await serviceWorker.pushManager.subscribe({
+        applicationServerKey: publicKeyResponse.data.publicKey,
+        userVisibleOnly: true,
+      });
+    }
+
+    // Sending the subscription to the backend to save in the database so we can use
+    // that subscription to send messages to the user
+    await api.post("/push/register", {
+      subscription,
+    });
+
+    // Just to test but we can send notifications to the user now from the backend
+    // await api.post("/push/send", {
+    //   subscription,
+    // });
+  });
 
 export function App() {
   return (
